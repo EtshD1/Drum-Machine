c07a29188a6842b7b4eb7517bbf38089920c87c3
EtshD1 <H79Pain@Gmail.com>
2021-05-02 05:22:51 +0200

    add onkeydown event

--- src/Components/DrumPad.tsx
+++ src/Components/DrumPad.tsx
@@ -1,13 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const DrumPad = ({ name }: { name: string }) => {
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  const handleClick = () => {
+  const handlePlay = () => {
     audioRef.current?.play();
   }
 
-  return (<div id={`${name}Container`} className="drum-pad" onClick={handleClick}>
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (e.key.toLocaleLowerCase() === name.toLocaleLowerCase()) {
+      audioRef.current?.play();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeydown);
+    return () => document.removeEventListener('keydown', handleKeydown);
+  }, []);
+
+  return (<div id={`${name}Container`} className="drum-pad" onClick={handlePlay}>
     {name}
     <audio ref={audioRef} id={name} className="clip" src={`./audio/${name}.wav`}></audio>
   </div>);
